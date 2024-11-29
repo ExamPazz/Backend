90b2731d82910c34381b5989c10825c1ca607b33
Obasi, Mary Olanma <olanmamary2@gmail.com>
2024-11-29 10:48:05 +0100

    Update GoogleAuthController.php

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\GoogleAuthFormRequest;
 use App\Services\GoogleAuthService;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+use App\Support\ApiResponse;
 
 
 class GoogleAuthController extends Controller
@@ -16,13 +17,11 @@ class GoogleAuthController extends Controller
         try {
             $user = (new GoogleAuthService())->setUpAuthentication($request);
             $token = JWTAuth::fromUser($user);
-        } catch (\Exception $e) {
-            return response()->json([
-                'error' => true,
-                'message' => $e->getMessage(),
-            ], 409);
-        }
 
-        return ($token) ? $this->respondWithToken($token) : $this->errorResponse('Unauthenticated token', 409);
+            return ApiResponse::success('Authentication successful', ['token' => $token]);
+
+        } catch (\Exception $e) {
+            return ApiResponse::failure($e->getMessage(), [], 409);
+        }
     }
 }
