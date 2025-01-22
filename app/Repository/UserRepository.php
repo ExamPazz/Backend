c944781e56aa46e39d01a99231acd40a0d39c17f
Obasi, Mary Olanma <olanmamary2@gmail.com>
2025-01-22 10:26:05 +0100

    Update UserRepository.php

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository;
 use App\Http\Requests\RegistrationRequest;
 use App\Models\User;
 use App\Models\UserProfile;
+use App\Models\Subscription;
+use App\Models\SubscriptionPlan;
 use Illuminate\Support\Facades\DB;
 
 class UserRepository
@@ -29,6 +31,17 @@ class UserRepository
                     'city' => $request->input('city'),
                     'age' => $request->input('age')
                 ]);
+
+                 // Assigning freemium subscription
+            $freemiumPlan = SubscriptionPlan::where('name', 'Freemium')->first();
+
+            if ($freemiumPlan) {
+                Subscription::create([
+                    'user_id' => $user->id,
+                    'subscription_plan_id' => $freemiumPlan->id,
+                    'expires_at' => now()->addDays(30), // Example: 30-day free period
+                ]);
+            }
             return $user;
         });
     }
