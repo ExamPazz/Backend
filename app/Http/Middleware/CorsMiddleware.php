0685c2b1247f298b7f7f9f749fe7dd5c001000fd
Obasi, Mary Olanma <olanmamary2@gmail.com>
2025-02-11 16:43:40 +0100

    Update CorsMiddleware.php

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -14,6 +14,6 @@ class CorsMiddleware
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-        }
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
 }
