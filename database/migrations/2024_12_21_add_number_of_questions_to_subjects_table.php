b6a65757ab02457d91d2c085143052ebd0352609
dybaay <ajakuth@gmail.com>
2025-02-28 14:29:08 +0100

    Update 2024_12_21_add_number_of_questions_to_subjects_table.php

--- database/migrations/2024_12_21_add_number_of_questions_to_subjects_table.php
+++ database/migrations/2024_12_21_add_number_of_questions_to_subjects_table.php
@@ -9,14 +9,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('subjects', function (Blueprint $table) {
-            // $table->integer('number_of_questions')->default(50)->after('name');
+            $table->integer('number_of_questions')->default(50)->after('name');
         });
     }
 
     public function down(): void
     {
         Schema::table('subjects', function (Blueprint $table) {
-            // $table->dropColumn('number_of_questions');
+            $table->dropColumn('number_of_questions');
         });
     }
 };
