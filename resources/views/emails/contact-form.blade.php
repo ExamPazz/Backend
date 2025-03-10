a2f5197cc0b757beb3ddf95f2752e404941fc30c
dybaay <ajakuth@gmail.com>
2025-03-10 17:26:40 +0100

    Update contact-form.blade.php

--- resources/views/emails/contact-form.blade.php
+++ resources/views/emails/contact-form.blade.php
@@ -39,28 +39,28 @@
     <div class="container">
         <div class="header">
             <h2>New Contact Form Submission</h2>
-            <p>Received on: {{ $submitted_at }}</p>
+            <p>Received on:{!!$submitted_at !!}</p>
         </div>
 
         <div class="content">
             <div class="field">
                 <div class="label">Name:</div>
-                <div>{{ $name }}</div>
+                <div> {!! $name !!}</div>
             </div>
 
             <div class="field">
                 <div class="label">Email:</div>
-                <div>{{ $email }}</div>
+                <div> {!! $email !!}</div>
             </div>
 
             <div class="field">
                 <div class="label">Phone Number:</div>
-                <div>{{ $phone_number }}</div>
+                <div>{!! $phone_number !!}</div>
             </div>
 
             <div class="field">
                 <div class="label">Message:</div>
-                <div>{{ $message }}</div>
+                <div>{!! $message !!}</div>
             </div>
         </div>
     </div>
